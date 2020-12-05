250a8e2fe0cb07a7256c58bb2a7852f7f71f22d5
Daryl.Xu <ziqiang_xu@qq.com>
2020-12-05 18:47:38 +0800

    feat: get 3d neighborhood from volume

--- lymphangioma_segmentation/image.py
+++ lymphangioma_segmentation/image.py
@@ -6,6 +6,9 @@ import numpy as np
 
 
 class Pixel:
+    """
+    volume存储顺序：height/dicom堆叠方向，row/x，column/y
+    """
     def __init__(self, row: int, col: int, height: int = None):
         # 笛卡尔（左手）坐标系
         self.row = row  # y
@@ -77,6 +80,29 @@ class Pixel:
         pixels.extend(pixels3_8)
 
         return pixels
+
+    def get_neighborhood_3d_arr(self, img: np.ndarray, half_width: int = 3,
+                                half_height: int = 1) -> np.ndarray:
+        def valid_index(test_value, upper):
+            assert 0 <= test_value < upper
+
+        shape = img.shape
+        height1 = self.height - half_height
+        height2 = self.height + half_height
+        valid_index(height1, shape[0])
+        valid_index(height2, shape[0])
+
+        row1 = self.row - half_width
+        row2 = self.row + half_width
+        valid_index(row1, shape[1])
+        valid_index(row2, shape[1])
+
+        column1 = self.col - half_width
+        column2 = self.col + half_width
+        valid_index(column1, shape[2])
+        valid_index(column2, shape[2])
+        arr: np.ndarray = img[height1:height2, row1:row2, column1:column2]
+        return arr.copy()
 
     def get_pixel(self, img: np.ndarray):
         assert img.ndim == 2
