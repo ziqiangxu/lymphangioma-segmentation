aba20ae5db2b27afdfa6056c9f9c861fef23ff95
Daryl.Xu <ziqiang_xu@qq.com>
2020-12-05 21:14:46 +0800

    feat: test for optimized threshold and seed points of some volume data

--- lymphangioma_segmentation/test/test_optimized_threshold.py
+++ lymphangioma_segmentation/test/test_optimized_threshold.py
@@ -2,8 +2,11 @@
 Author: Daryl.Xu
 E-mail: ziqiang_xu@qq.com
 """
+import os
+
 import numpy as np
 import matplotlib.pyplot as plt
+import public
 
 from lymphangioma_segmentation import segmentation
 from lymphangioma_segmentation.image import Pixel
@@ -31,6 +34,16 @@ SEEDS_P0.append(Pixel(342, 170, 15))
 
 PATIENT1 = 'p1.npy'
 SEEDS_P1 = list()
+SEEDS_P1.append(Pixel(200, 182, 2))
+SEEDS_P1.append(Pixel(212, 165, 3))
+SEEDS_P1.append(Pixel(227, 156, 4))
+SEEDS_P1.append(Pixel(223, 159, 5))
+SEEDS_P1.append(Pixel(237, 165, 6))
+SEEDS_P1.append(Pixel(256, 164, 7))
+SEEDS_P1.append(Pixel(272, 172, 8))
+SEEDS_P1.append(Pixel(277, 181, 9))
+SEEDS_P1.append(Pixel(276, 182, 10))
+SEEDS_P1.append(Pixel(295, 170, 11))
 
 
 def test_optimized_thresholds():
@@ -38,35 +51,58 @@ def test_optimized_thresholds():
         neighborhood_arr = seed.get_neighborhood_3d_arr(img, 10)
         reference_intensity = neighborhood_arr.mean()
         slice_ = seed.get_slice(img)
-        optimized_threshold = segmentation.get_optimized_threshold(
-            slice_, seed, reference_intensity, 0.1, True)
-        print(f'threshold: {optimized_threshold[0]}, '
-              f'trigger ratio: {optimized_threshold[1]}')
+        optimized_threshold, trigger_ratio = segmentation.get_optimized_threshold(
+            slice_, seed, reference_intensity, 0.5, True)
+        print(f'threshold: {optimized_threshold}, '
+              f'trigger ratio: {trigger_ratio}')
+        mask = segmentation.region_grow(slice_, seed, optimized_threshold)
+        save_path = os.path.join(log_path, f'mask_contours_{seed.height}.png')
+        public.draw_mask_contours(slice_, mask, save_path)
 
     # npy = np.load(DATA1)
+    # log_path = public.get_log_path('data1', True)
     # for s in SEEDS1:
     #     compute(npy, s)
 
-    npy0 = np.load(PATIENT0)
-    for s in SEEDS_P0:
-        compute(npy0, s)
+    # npy0 = np.load(PATIENT0)
+    # log_path = public.get_log_path('patient0', True)
+    # for s in SEEDS_P0:
+    #     compute(npy0, s)
+
+    npy1 = np.load(PATIENT1)
+    log_path = public.get_log_path('patient1', True)
+    for s in SEEDS_P1:
+        compute(npy1, s)
+
+    # npy2 = np.load(PATIENT2)
+    # log_path = public.get_log_path('patient2', True)
+    # for s in SEEDS_P2:
+    #     compute(npy2, s)
 
 
 def test_show_seeds():
-    img = np.load(PATIENT0)
-    # for i in range(img.shape[0]):
+    img: np.ndarray = np.load(PATIENT0)
     for s in SEEDS_P0:
-        slice_ = img[s.height]
+        slice_: np.ndarray = img[s.height]
         figure, axes = plt.subplots()
-        # figure.canvas.set_window_title(f'图片索引：{i}')
         axes.imshow(slice_, cmap='gray')
-
         rec = plt.Rectangle((s.col, s.row), 10, 10)
         axes.add_patch(rec)
 
         plt.show()
 
 
+def test_display_volume():
+    img: np.ndarray = np.load(PATIENT1)
+    for i in range(img.shape[0]):
+        slice_: np.ndarray = img[i]
+        figure, axes = plt.subplots()
+        figure.canvas.set_window_title(f'图片索引：{i}')
+        axes.imshow(slice_, cmap='gray')
+        plt.show()
+
+
 if __name__ == '__main__':
-    # test_optimized_thresholds()
-    test_show_seeds()
+    test_optimized_thresholds()
+    # test_show_seeds()
+    # test_display_volume()
